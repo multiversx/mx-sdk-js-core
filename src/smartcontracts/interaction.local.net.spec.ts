947d53d4e9aba4bd1b09d620ee22e72b7b578832
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-03-22 12:01:36 +0200

    Fix test, bump alpha version: 10.0.0-alpha.3.

--- src/smartcontracts/interaction.local.net.spec.ts
+++ src/smartcontracts/interaction.local.net.spec.ts
@@ -118,7 +118,7 @@ describe("test smart contract interactor", function () {
             OptionValue.newProvided(new U32Value(1)),
             OptionValue.newMissing(),
             OptionValue.newMissing(),
-            OptionValue.newMissing()
+            OptionalValue.newMissing()
         ]).withGasLimit(new GasLimit(30000000));
 
         let lotteryStatusInteraction = <Interaction>contract.methods.status([

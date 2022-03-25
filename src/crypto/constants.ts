32a32b377cb0eedb27c4adb8117b63e2e21163c1
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-03-25 21:29:04 +0200

    Remove files (moved to separate repository).

--- src/crypto/constants.ts
+++
@@ -1,5 +0,0 @@
-// In a future PR, improve versioning infrastructure for key-file objects in erdjs.
-export const Version = 4;
-export const CipherAlgorithm = "aes-128-ctr";
-export const DigestAlgorithm = "sha256";
-export const KeyDerivationFunction = "scrypt";

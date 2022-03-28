c954f5416860b125e8af0575b37f78abda1a5a4d
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-03-28 12:16:05 +0300

    Fix reference in package.json, fix workflows.

--- src/boundaryAdapters.ts
+++ src/boundaryAdapters.ts
@@ -2,6 +2,9 @@ import { Address } from "./address";
 import { ErrInvariantFailed } from "./errors";
 import { Signature } from "./signature";
 
+/**
+ * Adapts a signature created by other components (e.g. erdjs-walletcore, erdjs-hw-provider) to one understood by erdjs.
+ */
 export function adaptToSignature(obj: any): Signature {
     if (!obj.hex || typeof obj.hex() !== "string") {
         throw new ErrInvariantFailed("adaptToSignature: bad or missing hex()")
@@ -10,6 +13,9 @@ export function adaptToSignature(obj: any): Signature {
     return new Signature(obj.hex());
 }
 
+/**
+ * Adapts an address created by other components (e.g. erdjs-walletcore, erdjs-hw-provider) to one understood by erdjs.
+ */
 export function adaptToAddress(obj: any): Address {
     if (!obj.bech32 || typeof obj.bech32() !== "string") {
         throw new ErrInvariantFailed("adaptToSignature: bad or missing bech32()")

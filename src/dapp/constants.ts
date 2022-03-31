d4a7020a4edf2e366c798927f858ea3398e3a9e1
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-03-31 11:03:04 +0300

    Cleanup.

--- src/dapp/constants.ts
+++ src/dapp/constants.ts
@@ -1,6 +1,3 @@
-export const WALLET_PROVIDER_MAINNET = "https://wallet.elrond.com/dapp/init";
-export const WALLET_PROVIDER_TESTNET = "https://testnet-wallet.elrond.com/dapp/init";
-
 // Wallet intercom messages
 export const DAPP_DEFAULT_TIMEOUT = 5000;
 export const DAPP_MESSAGE_INIT = "dapp_message_init";
@@ -10,11 +7,3 @@ export const DAPP_MESSAGE_CONNECT_URL = "dapp_message_connect_url";
 export const DAPP_MESSAGE_LOG_OUT = "dapp_message_log_out";
 export const DAPP_MESSAGE_SEND_TRANSACTION_URL = "dapp_message_send_transaction_url";
 export const DAPP_MESSAGE_SIGN_TRANSACTION_URL = "dapp_message_sign_transaction_url";
-
-
-export const WALLET_PROVIDER_CONNECT_URL = "/hook/login";
-export const WALLET_PROVIDER_DISCONNECT_URL = "/hook/logout";
-export const WALLET_PROVIDER_SEND_TRANSACTION_URL = "/hook/transaction";
-export const WALLET_PROVIDER_SIGN_TRANSACTION_URL = "/hook/sign";
-export const WALLET_PROVIDER_CALLBACK_PARAM = "walletProviderStatus";
-export const WALLET_PROVIDER_CALLBACK_PARAM_TX_SIGNED = "transactionsSigned";
--- src/dapp/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @packageDocumentation
- * @module dapp
- */
-
-export * from "./constants";
-export * from "./interface";
-export * from "./walletProvider";
-export * from "./extensionProvider";

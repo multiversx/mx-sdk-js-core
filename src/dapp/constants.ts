5aa33ad733bcf64a982f2931fdac68edd67d9cdb
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-03-31 11:38:09 +0300

    Remove not used files.

--- src/dapp/constants.ts
+++
@@ -1,9 +0,0 @@
-// Wallet intercom messages
-export const DAPP_DEFAULT_TIMEOUT = 5000;
-export const DAPP_MESSAGE_INIT = "dapp_message_init";
-export const DAPP_MESSAGE_IS_CONNECTED = "dapp_message_is_connected";
-export const DAPP_MESSAGE_GET_ADDRESS = "dapp_message_get_address";
-export const DAPP_MESSAGE_CONNECT_URL = "dapp_message_connect_url";
-export const DAPP_MESSAGE_LOG_OUT = "dapp_message_log_out";
-export const DAPP_MESSAGE_SEND_TRANSACTION_URL = "dapp_message_send_transaction_url";
-export const DAPP_MESSAGE_SIGN_TRANSACTION_URL = "dapp_message_sign_transaction_url";
--- src/dapp/interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IDappMessageEvent extends MessageEvent {
-    data: {
-        type: string;
-        data: any;
-        error: string;
-    };
-}

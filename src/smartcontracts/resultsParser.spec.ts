6f5e426ab3fdffabb993b21ef301bbc81789bb36
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-04-03 21:57:39 +0300

    Added some tests.

--- src/smartcontracts/resultsParser.spec.ts
+++ src/smartcontracts/resultsParser.spec.ts
@@ -7,6 +7,9 @@ import { ResultsParser } from "./resultsParser";
 import { TransactionOnNetwork } from "../transactionOnNetwork";
 import { SmartContractResultItem, SmartContractResults } from "./smartContractResults";
 import { Nonce } from "../nonce";
+import { TransactionEvent, TransactionEventTopic, TransactionLogs } from "../transactionLogs";
+import { Address } from "../address";
+
 
 describe("test smart contract results parser", () => {
     let parser = new ResultsParser();
@@ -56,5 +59,68 @@ describe("test smart contract results parser", () => {
         assert.deepEqual(bundle.firstValue, new BigUIntValue(42));
         assert.deepEqual(bundle.secondValue, BytesValue.fromHex("abba"));
         assert.lengthOf(bundle.values, 2);
+    });
+
+    it("should parse contract outcome, on easily found result with return data", async () => {
+        let transaction = new TransactionOnNetwork({
+            results: new SmartContractResults([
+                new SmartContractResultItem({
+                    nonce: new Nonce(42),
+                    data: "@6f6b@03",
+                    returnMessage: "foobar"
+                })
+            ])
+        });
+
+        let bundle = parser.parseUntypedOutcome(transaction);
+        assert.deepEqual(bundle.returnCode, ReturnCode.Ok);
+        assert.equal(bundle.returnMessage, "foobar");
+        assert.deepEqual(bundle.values, [Buffer.from("03", "hex")]);
+    });
+
+    it("should parse contract outcome, on signal error", async () => {
+        let transaction = new TransactionOnNetwork({
+            logs: new TransactionLogs(
+                new Address(), 
+                [
+                    new TransactionEvent(
+                        new Address(), 
+                        "signalError", 
+                        [
+                            new TransactionEventTopic(Buffer.from("something happened").toString("base64"))
+                        ],
+                        `@${Buffer.from("user error").toString("hex")}@07`
+                    )    
+                ]
+            )
+        });
+
+        let bundle = parser.parseUntypedOutcome(transaction);
+        assert.deepEqual(bundle.returnCode, ReturnCode.UserError);
+        assert.equal(bundle.returnMessage, "something happened");
+        assert.deepEqual(bundle.values, [Buffer.from("07", "hex")]);
+    });
+
+    it("should parse contract outcome, on too much gas warning", async () => {
+        let transaction = new TransactionOnNetwork({
+            logs: new TransactionLogs(
+                new Address(), 
+                [
+                    new TransactionEvent(
+                        new Address(), 
+                        "writeLog", 
+                        [
+                            new TransactionEventTopic("QHRvbyBtdWNoIGdhcyBwcm92aWRlZCBmb3IgcHJvY2Vzc2luZzogZ2FzIHByb3ZpZGVkID0gNTk2Mzg0NTAwLCBnYXMgdXNlZCA9IDczMzAxMA==")
+                        ],
+                        Buffer.from("QDZmNmI=", "base64").toString()
+                    )    
+                ]
+            )
+        });
+
+        let bundle = parser.parseUntypedOutcome(transaction);
+        assert.deepEqual(bundle.returnCode, ReturnCode.Ok);
+        assert.equal(bundle.returnMessage, "@too much gas provided for processing: gas provided = 596384500, gas used = 733010");
+        assert.deepEqual(bundle.values, []);
     });
 });

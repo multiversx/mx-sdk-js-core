a66f6a0dc748b2dcc1ef009ceb5f7212272f2fd0
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-04-07 11:25:35 +0300

    Move deprecated proxy provider to contractWrappers (it depends on it).

--- src-network-providers/networkProvider/index.ts
+++ src-network-providers/networkProvider/index.ts
@@ -1,8 +1,5 @@
 export * from "./apiNetworkProvider";
 export * from "./proxyNetworkProvider";
 
-// The old providers.
-export * from "./deprecatedProxyProvider";
-
 // we do not export "./tokens"
 // we do not export "./transactions"

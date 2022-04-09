f13e771b104c67ec24d94b72c47eb1db202e877d
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-04-09 23:24:34 +0300

    Fix exports.

--- src-network-providers/index.ts
+++ src-network-providers/index.ts
@@ -1,16 +1,14 @@
-export { createProxyNetworkProvider, createApiNetworkProvider } from "./factory";
-export { INetworkProvider } from "./interface";
+export { ProxyNetworkProvider } from "./proxyNetworkProvider";
+export { ApiNetworkProvider } from "./apiNetworkProvider";
 
 export { AccountOnNetwork } from "./accounts";
-
-export { ContractQueryResponse } from "./contractQueryResponse";
-export { ContractResults, ContractResultItem } from "./contractResults";
-
 export { TransactionOnNetwork } from "./transactions";
 export { TransactionEvent, TransactionEventTopic } from "./transactionEvents";
 export { TransactionLogs } from "./transactionLogs";
 export { TransactionReceipt } from "./transactionReceipt";
 export { TransactionStatus } from "./transactionStatus";
+export { ContractQueryResponse } from "./contractQueryResponse";
+export { ContractResults, ContractResultItem } from "./contractResults";
 
 export { FungibleTokenOfAccountOnNetwork, NonFungibleTokenOfAccountOnNetwork } from "./tokens";
 export { DefinitionOfFungibleTokenOnNetwork, DefinitionOfTokenCollectionOnNetwork } from "./tokenDefinitions";

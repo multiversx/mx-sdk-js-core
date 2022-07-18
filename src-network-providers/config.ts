98b8b07594e03fe3d75ab5932f7ba5ba0b56511e
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-07-18 19:50:19 +0300

    Increase default axios timeout.

--- src-network-providers/config.ts
+++ src-network-providers/config.ts
@@ -3,7 +3,7 @@ import { IPagination } from "./interface";
 const JSONbig = require("json-bigint");
 
 export const defaultAxiosConfig = {
-    timeout: 1000,
+    timeout: 5000,
     // See: https://github.com/axios/axios/issues/983 regarding transformResponse
     transformResponse: [
         function (data: any) {

5b3117951f337e8f40771433d2426415ba1c0905
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2022-08-24 11:38:58 +0300

    fixed changelog, export encryptor/decryptor and prepared release

--- src-wallet/crypto/index.ts
+++ src-wallet/crypto/index.ts
@@ -1,5 +1,7 @@
 export * from "./constants";
 export * from "./encryptor";
 export * from "./decryptor";
+export * from "./pubkeyEncryptor";
+export * from "./pubkeyDecryptor";
 export * from "./encryptedData";
 export * from "./randomness";

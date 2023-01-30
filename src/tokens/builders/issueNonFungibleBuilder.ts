49242fa98f96bd6ed36399761c3d2d3d87348da3
andreibancioiu <andrei.bancioiu@gmail.com>
2023-01-30 14:34:08 +0200

    Sketch another builders (commented out).

---
+++ src/tokens/builders/issueNonFungibleBuilder.ts
@@ -0,0 +1,17 @@
+// IssuanceTransaction {
+//     Sender: <account address of the token manager>
+//     Receiver: erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u
+//     Value: 50000000000000000 # (0.05 EGLD)
+//     GasLimit: 60000000
+//     Data: "issueNonFungible" +
+//           "@" + <token name in hexadecimal encoding> +
+//           "@" + <token ticker in hexadecimal encoding> +
+//           "@" + <"canFreeze" hexadecimal encoded> + "@" + <"true" or "false" hexadecimal encoded> +
+//           "@" + <"canWipe" hexadecimal encoded> + "@" + <"true" or "false" hexadecimal encoded> +
+//           "@" + <"canPause" hexadecimal encoded> + "@" + <"true" or "false" hexadecimal encoded> +
+//           "@" + <"canTransferNFTCreateRole" hexadecimal encoded> + "@" + <"true" or "false" hexadecimal encoded> +
+//           "@" + <"canChangeOwner" hexadecimal encoded> + "@" + <"true" or "false" hexadecimal encoded> +
+//           "@" + <"canUpgrade" hexadecimal encoded> + "@" + <"true" or "false" hexadecimal encoded> +
+//           "@" + <"canAddSpecialRoles" hexadecimal encoded> + "@" + <"true" or "false" hexadecimal encoded> +
+//           ...
+// }

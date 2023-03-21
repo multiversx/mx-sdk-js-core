19dc566bfd5b85b5aab59ddbe5b8222234c86466
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-03-21 13:55:11 +0200

    @noble/ed25519 for sign & verify (user wallets).

---
+++ src-wallet/usersBenchmark.spec.ts
@@ -0,0 +1,53 @@
+import { utils } from "@noble/ed25519";
+import { assert } from "chai";
+import { UserPublicKey, UserSecretKey } from "./userKeys";
+
+describe("behchmark sign and verify", () => {
+
+    it.only("should sign and verify", async function () {
+        this.timeout(60000);
+
+        const n = 10000;
+        const secretKeys: UserSecretKey[] = [];
+        const publicKeys: UserPublicKey[] = [];
+        const messages: Buffer[] = [];
+        const goodSignatures: Buffer[] = [];
+
+        for (let i = 0; i < n; i++) {
+            const secretKey = new UserSecretKey(Buffer.from(utils.randomBytes(32)));
+            const publicKey = secretKey.generatePublicKey();
+            const message = Buffer.from(utils.randomBytes(256));
+
+            secretKeys.push(secretKey);
+            publicKeys.push(publicKey);
+            messages.push(message);
+        }
+
+        console.time("sign");
+
+        for (let i = 0; i < n; i++) {
+            const signature = secretKeys[i].sign(messages[i]);
+            goodSignatures.push(signature);
+        }
+
+        console.timeEnd("sign");
+
+        console.time("verify (good)");
+
+        for (let i = 0; i < n; i++) {
+            const ok = publicKeys[i].verify(messages[i], goodSignatures[i]);
+            assert.isTrue(ok);
+        }
+
+        console.timeEnd("verify (good)");
+
+        console.time("verify (bad)");
+
+        for (let i = 0; i < n; i++) {
+            const ok = publicKeys[i].verify(messages[messages.length - i - 1], goodSignatures[i]);
+            assert.isFalse(ok);
+        }
+
+        console.timeEnd("verify (bad)");
+    });
+});

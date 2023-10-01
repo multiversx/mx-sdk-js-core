382c9260ff5e963a8a70214ad12b03959b9a0a49
micha vie <micha@freeiam.com>
2023-10-01 15:39:52 +0200

    add endpoint only-owner test

---
+++ src/smartcontracts/typesystem/endpoint.spec.ts
@@ -0,0 +1,19 @@
+import { assert } from "chai";
+import { EndpointDefinition } from "./endpoint";
+
+
+describe("test endpoint", () => {
+    it('should should handle an only-owner modifier', async () => {
+      const actual = EndpointDefinition.fromJSON({
+        name: 'foo',
+        ownerOnly: true,
+        mutability: 'payable',
+        payableInTokens: [],
+        inputs: [],
+        outputs: [],
+      })
+
+      assert.isTrue(actual.modifiers.ownerOnly)
+      assert.isTrue(actual.modifiers.isOwnerOnly())
+    })
+});

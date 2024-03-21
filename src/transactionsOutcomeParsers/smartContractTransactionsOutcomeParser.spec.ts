403bcdbb733c7d169a9e05064106b6e7ade5a5f4
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-03-21 16:11:03 +0200

    Extra tests.

--- src/transactionsOutcomeParsers/smartContractTransactionsOutcomeParser.spec.ts
+++ src/transactionsOutcomeParsers/smartContractTransactionsOutcomeParser.spec.ts
@@ -1,6 +1,8 @@
 import { ContractResultItem, ContractResults, TransactionOnNetwork } from "@multiversx/sdk-network-providers";
+import BigNumber from "bignumber.js";
 import { assert } from "chai";
 import { TransactionsConverter } from "../converters/transactionsConverter";
+import { loadAbiRegistry } from "../testutils";
 import { SmartContractCallOutcome, TransactionOutcome } from "./resources";
 import { SmartContractTransactionsOutcomeParser } from "./smartContractTransactionsOutcomeParser";
 
@@ -42,6 +44,55 @@ describe("test smart contract transactions outcome parser", () => {
         const parsed = parser.parseExecute({ transactionOutcome });
 
         assert.deepEqual(parsed.values, [Buffer.from([42])]);
+        assert.equal(parsed.returnCode, "ok");
+        assert.equal(parsed.returnMessage, "ok");
+    });
+
+    it("parses execute outcome, with ABI (minimalistic)", async function () {
+        const parser = new SmartContractTransactionsOutcomeParser({
+            abi: await loadAbiRegistry("src/testdata/answer.abi.json"),
+        });
+
+        const parsed = parser.parseExecute({
+            transactionOutcome: new TransactionOutcome({
+                directSmartContractCallOutcome: new SmartContractCallOutcome({
+                    // For the sake of the test, let's say that we've called this function as a transaction, not as a query.
+                    function: "getUltimateAnswer",
+                    returnCode: "ok",
+                    returnMessage: "ok",
+                    returnDataParts: [Buffer.from([42])],
+                }),
+            }),
+        });
+
+        // At this moment, U64Value.valueOf() returns a BigNumber. This might change in the future.
+        assert.deepEqual(parsed.values, [new BigNumber("42")]);
+        assert.equal(parsed.returnCode, "ok");
+        assert.equal(parsed.returnMessage, "ok");
+    });
+
+    it("parses execute outcome, with ABI", async function () {
+        const parser = new SmartContractTransactionsOutcomeParser({
+            abi: await loadAbiRegistry("src/testdata/answer.abi.json"),
+        });
+
+        const transactionsConverter = new TransactionsConverter();
+        const transactionOnNetwork = new TransactionOnNetwork({
+            nonce: 7,
+            function: "getUltimateAnswer",
+            contractResults: new ContractResults([
+                new ContractResultItem({
+                    nonce: 8,
+                    data: "@6f6b@2a",
+                }),
+            ]),
+        });
+
+        const transactionOutcome = transactionsConverter.transactionOnNetworkToOutcome(transactionOnNetwork);
+        const parsed = parser.parseExecute({ transactionOutcome });
+
+        // At this moment, U64Value.valueOf() returns a BigNumber. This might change in the future.
+        assert.deepEqual(parsed.values, [new BigNumber("42")]);
         assert.equal(parsed.returnCode, "ok");
         assert.equal(parsed.returnMessage, "ok");
     });

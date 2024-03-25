a87c47f6ac4d77c0860c6083e422fa907513d25a
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-03-25 10:20:04 +0200

    Minor refactoring.

--- src/transactionsOutcomeParsers/transactionEventsParser.ts
+++ src/transactionsOutcomeParsers/transactionEventsParser.ts
@@ -7,38 +7,38 @@ interface IAbi {
 }
 
 export class TransactionEventsParser {
-    private readonly abi: IAbi;
     private readonly legacyResultsParser: ResultsParser;
+    private readonly abi: IAbi;
+    private readonly firstTopicIsIdentifier: boolean;
 
-    constructor(options: { abi: IAbi }) {
-        this.abi = options.abi;
+    constructor(options: { abi: IAbi; firstTopicIsIdentifier?: boolean }) {
         this.legacyResultsParser = new ResultsParser();
+        this.abi = options.abi;
+
+        // By default, we consider that the first topic is the event identifier.
+        // This is true for log entries emitted by smart contracts:
+        // https://github.com/multiversx/mx-chain-vm-go/blob/v1.5.27/vmhost/contexts/output.go#L283
+        this.firstTopicIsIdentifier =
+            options.firstTopicIsIdentifier === undefined ? true : options.firstTopicIsIdentifier;
     }
 
-    parseEvents(options: { events: TransactionEvent[]; firstTopicIsIdentifier?: boolean }): any[] {
-        const firstTopicIsIdentifier = options.firstTopicIsIdentifier;
+    parseEvents(options: { events: TransactionEvent[] }): any[] {
         const results = [];
 
         for (const event of options.events) {
-            const parsedEvent = this.parseEvent({ event, firstTopicIsIdentifier });
+            const parsedEvent = this.parseEvent({ event });
             results.push(parsedEvent);
         }
 
         return results;
     }
 
-    parseEvent(options: { event: TransactionEvent; firstTopicIsIdentifier?: boolean }): any {
-        // By default, we consider that the first topic is the event identifier.
-        // This is true for log entries emitted by smart contracts:
-        // https://github.com/multiversx/mx-chain-vm-go/blob/v1.5.27/vmhost/contexts/output.go#L283
-        const firstTopicIsIdentifier =
-            options.firstTopicIsIdentifier === undefined ? true : options.firstTopicIsIdentifier;
-
+    parseEvent(options: { event: TransactionEvent }): any {
         const topics = options.event.topics.map((topic) => Buffer.from(topic));
         const dataItems = options.event.dataItems.map((dataItem) => Buffer.from(dataItem));
         const eventDefinition = this.abi.getEvent(options.event.identifier);
 
-        if (firstTopicIsIdentifier) {
+        if (this.firstTopicIsIdentifier) {
             // Discard the first topic (not useful).
             topics.shift();
         }

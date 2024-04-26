f2f4faeb925cad5594c847e1dd072ce0ec04b366
Alexandru Popenta <alexandru.p9913@gmail.com>
2024-04-26 17:09:22 +0300

    wip: generate custom types

--- src/abiClassGenerator/generator.ts
+++ src/abiClassGenerator/generator.ts
@@ -1,5 +1,5 @@
 import { Address } from "../address";
-import { AbiRegistry } from "../smartcontracts";
+import { AbiRegistry, CustomType, EndpointDefinition, EnumType, StructType } from "../smartcontracts";
 import { Logger } from "../logger";
 import * as fs from "fs";
 import * as prettier from "prettier";
@@ -9,7 +9,7 @@ const path = require("node:path");
 const SMART_CONTRACT_FACTORY = "SmartContractTransactionsFactory";
 const FACTORY_CONFIG = "TransactionsFactoryConfig";
 const ABI_REGISTRY = "AbiRegistry";
-const PACKAGE = "multiversx/sdk-core";
+const CORE_PACKAGE = "multiversx/sdk-core";
 const TYPESCRIPT_LANGUAGE = "ts";
 
 export class Generator {
@@ -36,15 +36,15 @@ export class Generator {
     }
 
     generate() {
-        let tsGenerator: TypeScriptGenerator;
+        let generator: TypeScriptGenerator;
 
         if (this.targetLanguage === TYPESCRIPT_LANGUAGE) {
-            tsGenerator = this.initilizeTSGenerator();
+            generator = this.initilizeTSGenerator();
         } else {
             throw new Error(`Language "${this.targetLanguage}" not supported.`);
         }
 
-        tsGenerator.generate();
+        generator.generate();
     }
 
     private initilizeTSGenerator(): TypeScriptGenerator {
@@ -64,40 +64,84 @@ class TypeScriptGenerator {
     private readonly chainID: string;
     private generatedClass: string;
     private outputPath: string;
+    private customEnums: string;
+    private customStructs: string;
+    private customTypes: string;
 
     constructor(options: { abi: any; contractAddress: Address; chainID: string; outputPath: string }) {
         this.plainAbi = options.abi;
         this.abiRegistry = AbiRegistry.create(this.plainAbi);
+
+        // const type = <StructType>this.abiRegistry.getCustomType("ActionStatus");
+        // console.log(type);
+        // console.log("--------------------");
+
+        // for (const tp of type.getFieldsDefinitions()) {
+        //     console.log(tp.name);
+        //     console.log(tp.type);
+        // }
+
         this.contractAddress = options.contractAddress;
         this.chainID = options.chainID;
         this.generatedClass = "";
         this.outputPath = options.outputPath;
+        this.customEnums = "";
+        this.customStructs = "";
+        this.customTypes = "";
     }
 
     async generate() {
         const fileName = this.prepareFileName();
         const filePath = path.join(this.outputPath, fileName);
-
+        // return;
+        // const endpoint = this.abiRegistry.getEndpoint("proposeTransferExecuteEsdt");
+        // console.log(endpoint);
+        // console.log("-----------------------");
+        // console.log(endpoint.input);
+        // console.log("-----------------------");
+        // // console.log(endpoint.input[0].type.getTypeParameters());
+        // console.log(endpoint.input[1].type.getTypeParameters());
+        // // console.log(endpoint.input[2].type.getTypeParameters());
+        // // console.log(endpoint.input[3].type.getTypeParameters());
+        // // const res = this.prepareMethod(endpoint);
+        // // for (let item of res) {
+        // //     console.log(item);
+        // // }
+        const a = this.abiRegistry.getCustomType("ActionFullInfo");
+        console.log(a);
+        return;
+        // return;
         await this.generateClass();
-        this.saveFile(filePath);
+        this.saveFile(filePath, this.generatedClass);
+
+        await this.generateCustomTypes();
+        const typesPath = path.join(this.outputPath, "customTypes.ts");
+        this.saveFile(typesPath, this.customTypes);
+
         Logger.info(`Successfully generated ${fileName} at location ${filePath}.`);
+    }
+
+    async generateCustomTypes() {
+        this.createCustomTypes();
+        this.customTypes = await this.formatUsingPrettier(this.customEnums + "\n" + this.customStructs);
     }
 
     async generateClass() {
         const className = this.prepareClassName();
 
-        this.addImportDeclarations();
-        this.addClassDeclarations(className);
+        this.addImports();
+        this.addClassDefinition(className);
+
         this.addEndClassCurlyBracket();
 
-        await this.formatClass();
+        this.generatedClass = await this.formatUsingPrettier(this.generatedClass);
     }
 
-    saveFile(output: string) {
-        fs.writeFileSync(output, this.generatedClass);
+    saveFile(output: string, content: string) {
+        fs.writeFileSync(output, content);
     }
 
-    addImportDeclarations() {
+    addImports() {
         this.generatedClass +=
             this.createImportStatement(SMART_CONTRACT_FACTORY) +
             this.createImportStatement(FACTORY_CONFIG) +
@@ -105,7 +149,7 @@ class TypeScriptGenerator {
             this.createImportStatement("AbiRegistry");
     }
 
-    addClassDeclarations(className: string) {
+    addClassDefinition(className: string) {
         const classDefinition = `
         export class ${className} {
             private readonly factory: ${SMART_CONTRACT_FACTORY};
@@ -123,8 +167,217 @@ class TypeScriptGenerator {
         this.generatedClass += classDefinition;
     }
 
-    private createImportStatement(name: string, from?: string) {
-        const module = from ? from : PACKAGE;
+    // addMethodDefinition(endpoint: EndpointDefinition) {
+    //     let method = this.createDocString(endpoint);
+    // }
+
+    // private prepareMethod(endpoint: EndpointDefinition) {
+    //     const methodName = endpoint.name;
+    //     const inputs = endpoint.input;
+
+    //     let inputTuple: [string, string][] = [];
+    //     for (const input of inputs) {
+    //         inputTuple.push([input.name, input.type.getClassName()]);
+    //     }
+
+    //     return inputTuple;
+    // }
+
+    createCustomTypes() {
+        const contractTypes = this.abiRegistry.customTypes;
+
+        for (const type of contractTypes) {
+            if (type instanceof EnumType) {
+                const customEnum = this.createEnum(type);
+                this.customEnums += customEnum;
+            } else if (type instanceof StructType) {
+                const customStruct = this.createStruct(type);
+                this.customStructs += customStruct;
+            } else {
+                throw new Error(`Custom type of type ${typeof type} not supported`);
+            }
+        }
+    }
+
+    createEnum(customType: EnumType): string {
+        const enumName = customType.getName();
+        const variants = customType.variants;
+
+        let items: string = ``;
+        for (const item of variants) {
+            items += `${item.name} = ${item.discriminant},\n`;
+        }
+
+        return this.prepareEnumDefinition(enumName, items);
+    }
+
+    createStruct(customType: StructType): string {
+        const structName = customType.getName();
+        const fields = customType.getFieldsDefinitions();
+
+        let items: string = ``;
+        for (const field of fields) {
+            if (field.type.isGenericType()) {
+                const nativeType = this.mapOpenTypeToNativeType(field.type.getName());
+                if (nativeType === "nothing") {
+                    continue;
+                }
+
+                items += `${this.formatFieldName(field.name)}: ${nativeType};\n`;
+            } else {
+                items += `${this.formatFieldName(field.name)}: ${this.mapClosedTypeToNativeType(field.type.getName())};\n`;
+            }
+        }
+
+        return this.prepareTypeDefinition(structName, items);
+    }
+
+    private formatFieldName(name: string): string {
+        if (name.includes("_")) {
+            const words = name.split("_");
+
+            for (let i = 1; i < words.length; i++) {
+                words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+            }
+
+            return words.join("");
+        }
+        return name;
+    }
+
+    private prepareEnumDefinition(name: string, body: string): string {
+        return `export enum ${name} {
+            ${body}
+        }\n\n`;
+    }
+
+    private prepareTypeDefinition(name: string, body: string): string {
+        return `export type ${name} = {
+            ${body}
+        }\n\n`;
+    }
+
+    private mapClosedTypeToNativeType(closedType: string): string {
+        const number = "number";
+        const bigint = "bigint";
+
+        switch (closedType) {
+            case "u8":
+                return number;
+            case "i8":
+                return number;
+            case "u16":
+                return number;
+            case "i16":
+                return number;
+            case "u32":
+                return number;
+            case "i32":
+                return number;
+            case "u64":
+                return bigint;
+            case "i64":
+                return bigint;
+            case "BigUint":
+                return bigint;
+            case "Bigint":
+                return bigint;
+            case "bool":
+                return "boolean";
+            case "bytes":
+                return "Uint8Array";
+            case "Address":
+                return "Address";
+            case "H256":
+                return "Uint8Array";
+            case "utf-8 string":
+                return "string";
+            case "TokenIdentifier":
+                return "string";
+            case "EgldOrEsdtTokenIdentifier":
+                return "string";
+            case "CodeMetadata":
+                return "CodeMetadata";
+            case "nothing":
+                return "nothing";
+            case "AsyncCall":
+                return "nothing";
+            default:
+                // return "any";
+                throw new Error(`Invalid type "${closedType}"`);
+        }
+    }
+
+    private mapOpenTypeToNativeType(openType: string): string {
+        switch (openType) {
+            default:
+                return "any";
+            // case "u8":
+            //     return number;
+            // case "i8":
+            //     return number;
+            // case "u16":
+            //     return number;
+            // case "i16":
+            //     return number;
+            // case "u32":
+            //     return number;
+            // case "i32":
+            //     return number;
+            // case "u64":
+            //     return bigint;
+            // case "i64":
+            //     return bigint;
+            // case "BigUint":
+            //     return bigint;
+            // case "Bigint":
+            //     return bigint;
+            // case "bool":
+            //     return "boolean";
+            // case "bytes":
+            //     return "Uint8Array";
+            // case "Address":
+            //     return "Address";
+            // case "H256":
+            //     return "Uint8Array";
+            // case "utf-8 string":
+            //     return "string";
+            // case "TokenIdentifier":
+            //     return "string";
+            // case "EgldOrEsdtTokenIdentifier":
+            //     return "string";
+            // case "CodeMetadata":
+            //     return "CodeMetadata";
+            // case "nothing":
+            //     return "nothing";
+            // case "AsyncCall":
+            //     return "nothing";
+            // default:
+            //     throw new Error(`Invalid type "${openType}"`);
+        }
+    }
+
+    private createDocString(endpoint: any): string {
+        const docs: string[] = endpoint.docs;
+        if (!docs.length) {
+            return "";
+        }
+
+        let docString = "/**\n";
+
+        for (const line of docs) {
+            docString += `* ${line}\n`;
+        }
+
+        return docString + "*/\n";
+    }
+
+    private getAllEndpoints(): EndpointDefinition[] {
+        return this.abiRegistry.getEndpoints();
+    }
+
+    private createImportStatement(name: string, from?: string): string {
+        const module = from ? from : CORE_PACKAGE;
         return `import { ${name} } from "@${module}";\n`;
     }
 
@@ -154,8 +407,8 @@ class TypeScriptGenerator {
         this.generatedClass += "}\n";
     }
 
-    private async formatClass() {
-        this.generatedClass = await prettier.format(this.generatedClass, {
+    private async formatUsingPrettier(code: string) {
+        return await prettier.format(code, {
             parser: "typescript",
             singleQuote: false,
             trailingComma: "all",

6651a8723a1bfaf6b61b867446b23b783d25415d
Alexandru Popenta <alexandru.p9913@gmail.com>
2024-05-21 17:06:11 +0300

    use camel case for file name

--- src/abiClassGenerator/typescriptGenerator.ts
+++ src/abiClassGenerator/typescriptGenerator.ts
@@ -713,7 +713,7 @@ export class TypeScriptGenerator {
             return "customTypes.ts";
         }
 
-        return name.charAt(0).toLowerCase() + name.slice(1) + "customTypes.ts";
+        return name.charAt(0).toLowerCase() + name.slice(1) + "CustomTypes.ts";
     }
 
     private addEndClassCurlyBracket() {

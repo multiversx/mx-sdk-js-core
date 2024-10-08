f70457ee64b03b77851c07ee02ca51050b89bb7f
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-10-08 11:46:12 +0300

    Adjust integration tests.

--- src/transactionsOutcomeParsers/smartContractTransactionsOutcomeParser.main.net.spec.ts
+++ src/transactionsOutcomeParsers/smartContractTransactionsOutcomeParser.main.net.spec.ts
@@ -1,53 +1,136 @@
 import { assert } from "chai";
 import { promises } from "fs";
+import { TransactionsConverter } from "../converters/transactionsConverter";
 import { createMainnetProvider } from "../testutils/networkProviders";
 import { SmartContractTransactionsOutcomeParser } from "./smartContractTransactionsOutcomeParser";
 
 describe("test smart contract transactions outcome parser on mainnet", () => {
     const networkProvider = createMainnetProvider();
     const parser = new SmartContractTransactionsOutcomeParser();
+    const converter = new TransactionsConverter();
 
-    it("should parse (1)", async function () {
+    it("should parse (execute_success)", async function () {
         this.timeout(3600000);
-        await testRecords("src/testdata/transactions_01.mainnet.json");
-    });
 
-    it("should parse (2)", async function () {
-        this.timeout(3600000);
-        await testRecords("src/testdata/transactions_02.mainnet.json");
-    });
-
-    it("should parse (3)", async function () {
-        this.timeout(3600000);
-        await testRecords("src/testdata/transactions_02.mainnet.json");
-    });
-
-    async function testRecords(path: string) {
-        const content: string = await promises.readFile(path, { encoding: "utf8" });
-        const records = JSON.parse(content);
+        const records = await loadRecords("execute_success");
 
         for (let i = 0; i < records.length; i++) {
-            const { hash, kind } = records[i];
+            const { hash } = records[i];
+            console.log(i, hash);
+
             const transactionOnNetwork = await networkProvider.getTransaction(hash);
-            const outcome = parser.parseExecute({ transactionOnNetwork });
+            const transactionOutcome = converter.transactionOnNetworkToOutcome(transactionOnNetwork);
+            const parsedOutcomeGivenTransactionOutcome = parser.parseExecute({ transactionOutcome });
+            const parsedOutcomeGivenTransactionOnNetwork = parser.parseExecute({ transactionOnNetwork });
 
-            console.log(i, hash, kind);
-            console.log(hash, transactionOnNetwork.function, outcome.returnCode, outcome.returnMessage, outcome.values);
-
-            if (kind == "simple_move_balance") {
-                assert.equal(outcome.returnCode, "");
-                assert.equal(outcome.returnMessage, "");
-                assert.lengthOf(outcome.values, 0);
-            } else if (kind == "relayed_error" || kind == "transfer_execute_error" || kind == "execute_error") {
-                assert.isTrue(outcome.returnCode.length > 0);
-                assert.isTrue(outcome.returnMessage.length > 0);
-                assert.lengthOf(outcome.values, 0);
-            } else if (kind == "relayed_success" || kind == "transfer_execute_success" || kind == "execute_success") {
-                assert.equal(outcome.returnCode, "ok");
-                assert.equal(outcome.returnMessage, "ok");
-            } else {
-                assert.fail("unknown kind");
-            }
+            assert.deepEqual(parsedOutcomeGivenTransactionOutcome, parsedOutcomeGivenTransactionOnNetwork);
+            assert.equal(parsedOutcomeGivenTransactionOnNetwork.returnCode, "ok");
+            assert.equal(parsedOutcomeGivenTransactionOnNetwork.returnMessage, "ok");
         }
+    });
+
+    it("should parse (execute_error)", async function () {
+        this.timeout(3600000);
+
+        const records = await loadRecords("execute_error");
+
+        for (let i = 0; i < records.length; i++) {
+            const { hash } = records[i];
+            console.log(i, hash);
+
+            const transactionOnNetwork = await networkProvider.getTransaction(hash);
+            const transactionOutcome = converter.transactionOnNetworkToOutcome(transactionOnNetwork);
+            const parsedOutcomeGivenTransactionOutcome = parser.parseExecute({ transactionOutcome });
+            const parsedOutcomeGivenTransactionOnNetwork = parser.parseExecute({ transactionOnNetwork });
+
+            assert.deepEqual(parsedOutcomeGivenTransactionOutcome, parsedOutcomeGivenTransactionOnNetwork);
+            assert.isTrue(parsedOutcomeGivenTransactionOnNetwork.returnCode.length > 0);
+            assert.isTrue(parsedOutcomeGivenTransactionOnNetwork.returnMessage.length > 0);
+            assert.lengthOf(parsedOutcomeGivenTransactionOnNetwork.values, 0);
+        }
+    });
+
+    it("should parse (transfer_execute_success)", async function () {
+        this.timeout(3600000);
+
+        const records = await loadRecords("transfer_execute_success");
+
+        for (let i = 0; i < records.length; i++) {
+            const { hash } = records[i];
+            console.log(i, hash);
+
+            const transactionOnNetwork = await networkProvider.getTransaction(hash);
+            const transactionOutcome = converter.transactionOnNetworkToOutcome(transactionOnNetwork);
+            const parsedOutcomeGivenTransactionOutcome = parser.parseExecute({ transactionOutcome });
+            const parsedOutcomeGivenTransactionOnNetwork = parser.parseExecute({ transactionOnNetwork });
+
+            assert.deepEqual(parsedOutcomeGivenTransactionOutcome, parsedOutcomeGivenTransactionOnNetwork);
+            assert.equal(parsedOutcomeGivenTransactionOnNetwork.returnCode, "ok");
+            assert.equal(parsedOutcomeGivenTransactionOnNetwork.returnMessage, "ok");
+        }
+    });
+
+    it("should parse (transfer_execute_error)", async function () {
+        this.timeout(3600000);
+
+        const records = await loadRecords("transfer_execute_error");
+
+        for (let i = 0; i < records.length; i++) {
+            const { hash } = records[i];
+            console.log(i, hash);
+
+            const transactionOnNetwork = await networkProvider.getTransaction(hash);
+            const transactionOutcome = converter.transactionOnNetworkToOutcome(transactionOnNetwork);
+            const parsedOutcomeGivenTransactionOutcome = parser.parseExecute({ transactionOutcome });
+            const parsedOutcomeGivenTransactionOnNetwork = parser.parseExecute({ transactionOnNetwork });
+
+            assert.deepEqual(parsedOutcomeGivenTransactionOutcome, parsedOutcomeGivenTransactionOnNetwork);
+            assert.isTrue(parsedOutcomeGivenTransactionOnNetwork.returnCode.length > 0);
+            assert.isTrue(parsedOutcomeGivenTransactionOnNetwork.returnMessage.length > 0);
+            assert.lengthOf(parsedOutcomeGivenTransactionOnNetwork.values, 0);
+        }
+    });
+
+    it("should parse (relayed_success)", async function () {
+        this.timeout(3600000);
+
+        const records = await loadRecords("relayed_success");
+
+        for (let i = 0; i < records.length; i++) {
+            const { hash } = records[i];
+            console.log(i, hash);
+
+            const transactionOnNetwork = await networkProvider.getTransaction(hash);
+            const parsedOutcomeGivenTransactionOnNetwork = parser.parseExecute({ transactionOnNetwork });
+
+            assert.equal(parsedOutcomeGivenTransactionOnNetwork.returnCode, "ok");
+            assert.equal(parsedOutcomeGivenTransactionOnNetwork.returnMessage, "ok");
+        }
+    });
+
+    it("should parse (relayed_error)", async function () {
+        this.timeout(3600000);
+
+        const records = await loadRecords("relayed_error");
+
+        for (let i = 0; i < records.length; i++) {
+            const { hash } = records[i];
+            console.log(i, hash);
+
+            const transactionOnNetwork = await networkProvider.getTransaction(hash);
+            const parsedOutcomeGivenTransactionOnNetwork = parser.parseExecute({ transactionOnNetwork });
+
+            assert.isTrue(parsedOutcomeGivenTransactionOnNetwork.returnCode.length > 0);
+            assert.isTrue(parsedOutcomeGivenTransactionOnNetwork.returnMessage.length > 0);
+            assert.lengthOf(parsedOutcomeGivenTransactionOnNetwork.values, 0);
+        }
+    });
+
+    async function loadRecords(kind: string): Promise<any[]> {
+        const path = "src/testdata/transactions.mainnet.json";
+        const content: string = await promises.readFile(path, { encoding: "utf8" });
+        const records = JSON.parse(content);
+        const recordsFiltered = records.filter((record: any) => record.kind == kind);
+        return recordsFiltered;
     }
 });

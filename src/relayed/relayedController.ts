3becf91cfdfdf9810f5df26ec8f78888a853f578
danielailie <danielailie12@gmail.com>
2024-11-14 15:08:40 +0200

    Add relayed folder

---
+++ src/relayed/relayedController.ts
@@ -0,0 +1,41 @@
+import { IAccount } from "../accounts/interfaces";
+import { Transaction } from "../transaction";
+import { TransactionComputer } from "../transactionComputer";
+import { TransactionsFactoryConfig } from "../transactionsFactories";
+import { RelayedTransactionsFactory } from "./relayedTransactionsFactory";
+import { CreateV1RelayTransactionInput, CreateV2RelayTransactionInput } from "./resources";
+
+export class RelayedController {
+    private factory: RelayedTransactionsFactory;
+    private txComputer: TransactionComputer;
+
+    /**
+     * The transactions are created from the perspective of the relayer.
+     * The 'sender' represents the relayer.
+     */
+    constructor(options: { chainID: string }) {
+        this.factory = new RelayedTransactionsFactory({
+            config: new TransactionsFactoryConfig(options),
+        });
+        this.txComputer = new TransactionComputer();
+    }
+
+    async createRelayedV1Transaction(sender: IAccount, options: CreateV1RelayTransactionInput): Promise<Transaction> {
+        const transaction = this.factory.createRelayedV1Transaction({ ...options, relayerAddress: sender.address });
+
+        transaction.nonce = options.nonce;
+        transaction.signature = await sender.sign(this.txComputer.computeBytesForSigning(transaction));
+
+        return transaction;
+    }
+
+    async createRelayedV2Transaction(sender: IAccount, options: CreateV2RelayTransactionInput): Promise<Transaction> {
+        const transaction = this.factory.createRelayedV2Transaction({ ...options, relayerAddress: sender.address });
+
+        transaction.nonce = options.nonce;
+        transaction.gasLimit = BigInt(0);
+        transaction.signature = await sender.sign(this.txComputer.computeBytesForSigning(transaction));
+
+        return transaction;
+    }
+}

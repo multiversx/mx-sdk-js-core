35839745604c1626b611eaa0ede059edd76fa883
Alexandru Popenta <alexandru.p9913@gmail.com>
2025-05-22 09:06:38 +0300

    governance controller and outcome parser

--- src/governance/resources.ts
+++ src/governance/resources.ts
@@ -35,9 +35,60 @@ export type ChangeConfigInput = {
     minPassThreshold: number;
 };
 
-export type ProposeProposalOutcome = {
+export type NewProposalOutcome = {
     proposalNonce: number;
     commitHash: string;
     startVoteEpoch: number;
     endVoteEpoch: number;
+};
+
+export type VoteOutcome = {
+    proposalNonce: number;
+    vote: string;
+    totalStake: bigint;
+    votingPower: bigint;
+};
+
+export type DelegateVoteOutcome = {
+    proposalNonce: number;
+    vote: string;
+    voter: Address;
+    userStake: bigint;
+    votingPower: bigint;
+};
+
+export type CloseProposalOutcome = {
+    commitHash: string;
+    passed: boolean;
+};
+
+export type GovernanceConfig = {
+    proposalFee: bigint;
+    minQuorum: number;
+    minPassThreshold: number;
+    minVetoThreshold: number;
+    lastProposalNonce: number;
+};
+
+export type ProposalInfo = {
+    cost: bigint;
+    commitHash: string;
+    nonce: number;
+    issuer: Address;
+    startVoteEpoch: number;
+    endVoteEpoch: number;
+    quorumStake: bigint;
+    numYesVotes: bigint;
+    numNoVotes: bigint;
+    numVetoVotes: bigint;
+    numAbstainVotes: bigint;
+    isClosed: boolean;
+    isPassed: boolean;
+};
+
+export type DelegatedVoteInfo = {
+    usedStake: bigint;
+    usedPower: bigint;
+    totalStake: bigint;
+    totalPower: bigint;
 };
